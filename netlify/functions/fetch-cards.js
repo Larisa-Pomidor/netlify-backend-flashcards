0a063ca8a77b38b076234b4d4304525b21f66eea
Larisa Pomidor <nikakinikakin@gmail.com>
2024-10-07 12:37:47 +0300

    Add ssl

--- netlify/functions/fetch-cards.js
+++ netlify/functions/fetch-cards.js
@@ -6,7 +6,10 @@ exports.handler = async (event, context) => {
     host: process.env.DB_HOST,      
     database: process.env.DB_NAME,   
     password: process.env.DB_PASSWORD,
-    port: 5432,                      
+    port: 5432,  
+    ssl: {
+        rejectUnauthorized: true
+    }                    
   });
 
   try {
