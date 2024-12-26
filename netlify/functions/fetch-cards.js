832b73f5442d65abb2e87352eab28e2f58a9c057
Larisa Pomidor <nikakinikakin@gmail.com>
2024-12-26 16:20:59 +0200

    Fix result bug

--- netlify/functions/fetch-cards.js
+++ netlify/functions/fetch-cards.js
@@ -42,7 +42,7 @@ exports.handler = async (event, context) => {
                 return {
                     statusCode: 200,
                     headers,
-                    body: JSON.stringify({ count: result })
+                    body: JSON.stringify({ count: result.rows[0].count })
                 };
             }
 
