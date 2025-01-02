5a4b91a514ff8d6d20cd43a32e015ff7e32646a7
Larisa Pomidor <nikakinikakin@gmail.com>
2025-01-02 22:50:54 +0200

    Fix minor issue

--- netlify/functions/fetch-spelling.js
+++ netlify/functions/fetch-spelling.js
@@ -68,7 +68,7 @@ exports.handler = async (event, context) => {
             let query;
             let values;
 
-            if (!word && !transcription) {
+            if (!word && !translation) {
                 query = 'UPDATE spelling SET score = $2 WHERE id = $1 RETURNING *';
                 values = [id, score];
             } else {
