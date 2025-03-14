07263e010ef45fc8e9540719522bd27373f44b53
Larisa Pomidor <nikakinikakin@gmail.com>
2025-03-14 14:25:53 +0200

    Modify Access-Control-Allow-Methods

--- netlify/functions/calendar.js
+++ netlify/functions/calendar.js
@@ -15,7 +15,7 @@ exports.handler = async (event, context) => {
     const headers = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Methods': 'GET, DELETE, POST, PUT',
     };
 
     if (event.httpMethod === 'OPTIONS') {
